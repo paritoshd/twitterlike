6acbb04fda1b25e6a0689691b64581522370a1d7
Paritosh Das <pdas.rke@gmail.com>
2017-07-24 05:53:26 +0530

    Changed path2

--- twitterlikepages/views.py
+++ twitterlikepages/views.py
@@ -38,16 +38,16 @@ def twitterView(request):
             if request.method == 'POST':
                 request_json_data = json.loads(request.body)
                 tempFileName = "data" + "/" + request_json_data['userid'] + ".json"
-                with open(os.path.join("app/twitterlikepages/static/", tempFileName), 'r') as file_json_data:
+                with open(os.path.join("app/twitterlike/twitterlikepages/static/", tempFileName), 'r') as file_json_data:
                     file_data = json.load(file_json_data)
                     new_entry = {'type': request_json_data['type'], 'retweetedby': '', 'replyto': request_json_data['replyto'],  'details': {'id': '11', 'name': 'Paritosh Das', 'userid': 'paritoshdas', 'aliasid': 'ParitoshDas', 'profile_pic': '../static/images/paritoshprofile.jpg'}, 'messsage': request_json_data['message'], 'timestamp': request_json_data['timestamp']}
                     file_data["tweets"].insert(0, new_entry)
-                with open(os.path.join("app/twitterlikepages/static/", tempFileName), 'w') as file_write_data:
+                with open(os.path.join("app/twitterlike/twitterlikepages/static/", tempFileName), 'w') as file_write_data:
                     file_write_data.write(json.dumps(file_data))
         return HttpResponse(status=200)
     else:
         jsonFileName = "data" + "/" + path + ".json"
-        with open(os.path.join("app/twitterlikepages/static/", jsonFileName)) as json_data:
+        with open(os.path.join("app/twitterlike/twitterlikepages/static/", jsonFileName)) as json_data:
             data  = json.load(json_data)
         return render(request, 'twittermain.html', {'loggedInUser': loggedInUser, 'loggedInUserName': 'Paritosh Das', 'currentUser': currentUser, 'data': json.dumps(data)})
 
